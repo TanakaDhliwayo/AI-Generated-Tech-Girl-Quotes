edd2df3619333896bdbba95b4c4d0fb3888d853c
Tanaka Dhliwayo <taniedhliwayocodes@gmail.com>
2025-03-23 15:30:49 +0200

    added user experince

--- src/script.js
+++ src/script.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "0044073784o23b1bead22fat05f51ae8";
   let context =
-    "You are a women in tech advocate , and women in tech who loves motivating women in tech. You are boss lady and a key board queen . You mission is to generate a short 4 line max quote in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the quotes. Sign the poem with 'Tanie Codes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a women in tech advocate , and women in tech who loves motivating women in tech. You are boss lady and a key board queen . You mission is to generate a short 4 line max quote in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do NOT include a title to the quotes. Sign the poem with 'Tanie Codes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a Tech Girl quotes about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
