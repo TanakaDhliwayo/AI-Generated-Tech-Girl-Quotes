ff5d3c4dc0b5853d218f7ad8f7a110714fec0c62
Tanaka Dhliwayo <taniedhliwayocodes@gmail.com>
2025-03-23 15:16:43 +0200

    Added an API to the javaScript

--- src/script.js
+++ src/script.js
@@ -1,13 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
-  alert("Hello");
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "0044073784o23b1bead22fat05f51ae8";
+  let context =
+    "You are a women in tech advocate , and women in tech who loves motivating women in tech. You are boss lady and a key board queen . You mission is to generate a short 4 line max quote in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the quotes. Sign the poem with 'Tanie Codes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a Tech Girl quotes about ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a Tech Girl Quote ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
