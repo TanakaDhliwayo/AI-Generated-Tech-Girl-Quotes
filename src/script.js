2f067c333494cc17af938383081d4fcd6d9295ce
Tanaka Dhliwayo <taniedhliwayocodes@gmail.com>
2025-03-23 14:15:33 +0200

    added Javascipt without api

--- src/script.js
+++ src/script.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+  alert("Hello");
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
